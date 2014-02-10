396112a45ada86c5829e9ef20fb32b0c99ed5b3e
Philipp Fehre <philipp.fehre@googlemail.com>
2014-02-10 17:01:48 +0100

    test neighbours

--- test/game_of_live_test.js
+++ test/game_of_live_test.js
@@ -28,6 +28,9 @@ Cell.prototype.applyRules = function () {
 
 function Board(numRows, numColumns) {
   var rows = []
+  var that = this
+  var maxRowIndex = numRows - 1
+  var maxColumnIndex = numColumns - 1
 
   function createRow() {
     var row = []
@@ -44,7 +47,43 @@ function Board(numRows, numColumns) {
   this.at = function (x, y) {
     return rows[x][y]
   }
+
+  this.assignNeighbourCount = function () {
+    rows.forEach(function (row, x) {
+      row.forEach(function (cell, y) {
+        var neighbourCells = neighbourCellsFor(x, y)
+        cell.neighbours = neighbourCells.reduce(function (acc, cell) {
+          if (cell.isAlive()) acc++
+          return acc
+        }, 0)
+      })
+    })
+  }
+
+  function neighbourCellsFor(x, y) {
+    var positionMap = [[-1, -1], [0, -1], [1, -1],
+                       [-1, 0],           [1, 0],
+                       [-1, 1],  [0, 1],  [1, 1]]
+    var neighbourCells = []
+    positionMap.forEach(function (posDelta) {
+      var dx = x + posDelta[0]
+      if (dx > maxRowIndex) {
+        dx = 0
+      } else if (dx < 0) {
+        dx = maxRowIndex
+      }
+      var dy = y + posDelta[1]
+      if (dy > maxColumnIndex) {
+        dy = 0
+      } else if (dy < 0) {
+        dy = maxColumnIndex
+      }
+      neighbourCells.push(that.at(dx, dy))
+    })
+    return neighbourCells
+  }
 }
+
 
 describe("Game of Life", function () {
   describe("Cell rules", function () {
@@ -79,6 +118,15 @@ describe("Game of Life", function () {
     it("is filled with dead cell at start", function () {
       var board = new Board(3, 3)
       board.at(0, 0).isAlive().should.not.be.true
+    })
+
+    it("assigns living neighbours", function () {
+      var board = new Board(3, 3)
+      board.at(0, 0).revive()
+      board.assignNeighbourCount()
+      board.at(0, 0).neighbours.should.be.equal(0)
+      board.at(1, 1).neighbours.should.be.equal(1)
+      board.at(2, 2).neighbours.should.be.equal(1)
     })
   })
 })
