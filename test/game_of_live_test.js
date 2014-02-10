457aa12515474db406843d9c81fa7d28e5487fae
Philipp Fehre <philipp.fehre@googlemail.com>
2014-02-10 12:39:44 +0100

    2. rule

--- test/game_of_live_test.js
+++ test/game_of_live_test.js
@@ -23,6 +23,8 @@ function Cell() {
 Cell.prototype.applyRules = function () {
   if (this.neighbours < 2) {
     this.die()
+  } else if (this.neighbours === 3) {
+    this.revive()
   }
 }
 
@@ -49,6 +51,12 @@ describe("Game of Life", function () {
         cell.neighbours = 1
         cell.applyRules()
         cell.isAlive().should.be.not.be.true
+      })
+
+      it("is revived with exactly 3 living neighbours if dead", function () {
+        cell.neighbours = 3
+        cell.applyRules()
+        cell.isAlive().should.be.be.true
       })
     })
   })
